cedcf462e5aaebd7c2084439137a891157c947fb
nakai kensuke <nakaikensuke@nakainoMacBook-Air.local>
2021-02-23 12:22:27 +0900

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_164359) do
+ActiveRecord::Schema.define(version: 2021_02_23_031009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "body", null: false
+    t.string "post_image"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
