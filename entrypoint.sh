c2ea3a2b66509d4651aaa199288d5af5d0824ce9
kenchaso <kenchasonakai@gmail.com>
2022-11-23 07:24:51 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
+# 自分のアプリに合わせて必要なコマンドを修正してください
 RAILS_ENV=production bin/webpack
-RAILS_ENV=production bin/rails assets:precompile
 RAILS_ENV=production bin/rails db:migrate
 
 rm -f tmp/pids/server.pid && bin/rails s -e production
