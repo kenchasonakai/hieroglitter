79ace0668077d03e883735050da2336ebede1dff
kenchaso <kenchasonakai@gmail.com>
2022-11-24 18:45:15 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-RAILS_ENV=production bin/rails db:migrate
-RAILS_ENV=production bin/rails db:seed
+bin/rails db:migrate
+bin/rails db:seed
 
-rm -f tmp/pids/server.pid && bin/rails s -e production
+rm -f tmp/pids/server.pid && bin/rails s
